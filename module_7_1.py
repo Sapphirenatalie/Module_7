5016f1b01689410d3c37030560cc5df2c18e2fe7
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-07-19 11:58:36 +0300

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -3,3 +3,25 @@ file = open(file_name, 'r')
 file_content = file.read()
 print(file_content)
 file.close()
+
+
+# Создайте в директории проекта текстовый файл с расширением txt
+# Добавьте в этот файл следующий текст
+# My soul is dark - Oh! quickly string
+# The harp I yet can brook to hear;
+# And let thy gentle fingers fling
+# Its melting murmurs o'er mine ear.
+# If in this heart a hope be dear,
+# That sound shall charm it forth again:
+# If in these eyes there lurk a tear,
+# 'Twill flow, and cease to burn my brain.
+# But bid the strain be wild and deep,
+# Nor let thy notes of joy be first:
+# I tell thee, minstrel, I must weep,
+# Or else this heavy heart will burst;
+# For it hath been by sorrow nursed,
+# And ached in sleepless silence, long;
+# And now 'tis doomed to know the worst,
+# And break at once - or yield to song.
+# Создайте переменную с этим файлом
+# Распечатайте содержимое текстового файла в консоль
